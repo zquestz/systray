44b7ef210b9e99fe05fe8f589334bdf5ca550f64
Andy Williams <andy@andy.xyz>
2022-02-04 17:09:24 -0800

    Remove lots of deps by replacing the error log line with fmt

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/fyne-io/systray
 
 go 1.13
 
-require (
-	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7
-	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9
-)
+require golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9
--- systray.go
+++ systray.go
@@ -6,13 +6,9 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
-
-	"github.com/getlantern/golog"
 )
 
 var (
-	log = golog.LoggerFor("systray")
-
 	systrayReady  func()
 	systrayExit   func()
 	menuItems     = make(map[uint32]*MenuItem)
@@ -233,7 +229,7 @@ func systrayMenuItemSelected(id uint32) {
 	item, ok := menuItems[id]
 	menuItemsLock.RUnlock()
 	if !ok {
-		log.Errorf("No menu item with ID %v", id)
+		fmt.Printf("No menu item with ID %v", id)
 		return
 	}
 	select {
