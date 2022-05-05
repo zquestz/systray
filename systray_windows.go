be7378015340a09892b35f32db244c2263b43652
Daniel <dhaavi@users.noreply.github.com>
2022-05-05 10:15:36 +0200

    Use separate atomic bool as init guard for winTray

--- systray_windows.go
+++ systray_windows.go
@@ -1,3 +1,4 @@
+//go:build windows
 // +build windows
 
 package systray
@@ -15,6 +16,7 @@ import (
 	"syscall"
 	"unsafe"
 
+	"github.com/tevino/abool"
 	"golang.org/x/sys/windows"
 )
 
@@ -208,11 +210,13 @@ type winTray struct {
 
 	wmSystrayMessage,
 	wmTaskbarCreated uint32
+
+	initialized *abool.AtomicBool
 }
 
 // isReady checks if the tray as already been initialized. It is not goroutine safe with in regard to the initialization function, but prevents a panic when functions are called too early.
 func (t *winTray) isReady() bool {
-	return t.instance != windows.Handle(0)
+	return t.initialized.IsSet()
 }
 
 // Loads an image from file and shows it in tray.
@@ -260,7 +264,9 @@ func (t *winTray) setTooltip(src string) error {
 	return t.nid.modify()
 }
 
-var wt winTray
+var wt = winTray{
+	initialized: abool.New(),
+}
 
 // WindowProc callback function that processes messages sent to a window.
 // https://msdn.microsoft.com/en-us/library/windows/desktop/ms633573(v=vs.85).aspx
@@ -276,6 +282,7 @@ func (t *winTray) wndProc(hWnd windows.Handle, message uint32, wParam, lParam ui
 	)
 	switch message {
 	case WM_CREATE:
+		t.initialized.Set()
 		systrayReady()
 	case WM_COMMAND:
 		menuItemId := int32(wParam)
