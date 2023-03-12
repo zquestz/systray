131fb7023cef7769977e262ae3e1cdd2eb7ef074
Andy Williams <andy@andy.xyz>
2023-03-12 21:14:47 +0000

    Fix submenu separators on Linux

--- example/main.go
+++ example/main.go
@@ -50,6 +50,7 @@ func onReady() {
 		subMenuTop := systray.AddMenuItem("SubMenuTop", "SubMenu Test (top)")
 		subMenuMiddle := subMenuTop.AddSubMenuItem("SubMenuMiddle", "SubMenu Test (middle)")
 		subMenuBottom := subMenuMiddle.AddSubMenuItemCheckbox("SubMenuBottom - Toggle Panic!", "SubMenu Test (bottom) - Hide/Show Panic!", false)
+		subMenuMiddle.AddSeparator()
 		subMenuBottom2 := subMenuMiddle.AddSubMenuItem("SubMenuBottom - Panic!", "SubMenu Test (bottom)")
 
 		systray.AddSeparator()
